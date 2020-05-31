0cb2a33747ed6dc4cf78852438fc98f003a4f507
Abdelrahman <abdo04590@gmail.com>
2020-06-01 00:14:18 +0200

    third edition

--- js/custom.js
+++ js/custom.js
@@ -6,4 +6,17 @@ $(document).ready(function(){
         $('.meet-features .row .d-none').fadeIn(3000).removeClass('d-none');
         $(this).addClass('d-none');
     });
+    // Parent settings select options
+for( var i = 1960 ; i <= 2020 ; i++)
+{
+	$("<option>" , {value: i, text: i}).appendTo($("#year"))
+}	
+for( var i = 1 ; i <= 12 ; i++)
+{
+	$("<option>" , {value: i, text: i}).appendTo($("#month"))
+}	
+for( var i = 1 ; i <= 31 ; i++)
+{
+	$("<option>" , {value: i, text: i}).appendTo($("#day"))
+}
 });
